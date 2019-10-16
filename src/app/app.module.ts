1407822afaf19ab412d5033edd16870a0e4738aa
aesthellar <m_dong@uncg.edu>
2019-10-16 13:59:15 -0400

    added all components. styled about and contact page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    AboutComponent,
+    ResumeComponent,
+    WorkComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-        { path: '', component: HomeComponent }
+      { path: '', component: AboutComponent },
+      { path: 'work', component: WorkComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'resume', component: ResumeComponent }
     ])
   ],
   providers: [],
